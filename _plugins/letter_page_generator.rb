6f17d0071c3723026df7081f1df998040e0ac5ce
junsan1 <junsan1>
2024-06-23 23:07:19 +0300

    First articles.

---
+++ _plugins/letter_page_generator.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  class LetterPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      languages = ['en', 'ru', 'zh']
+      letters = ('a'..'z').to_a + ('а'..'я').to_a
+
+      languages.each do |lang|
+        letters.each do |letter|
+          site.pages << LetterPage.new(site, site.source, "#{lang}/pedia", letter, lang)
+        end
+      end
+    end
+  end
+
+  class LetterPage < Page
+    def initialize(site, base, dir, letter, lang)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = "#{letter}.md"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'letter_index.html')
+      self.data['title'] = "Topics starting with #{letter.upcase}"
+      self.data['letter'] = letter
+      self.data['lang'] = lang
+    end
+  end
+end
