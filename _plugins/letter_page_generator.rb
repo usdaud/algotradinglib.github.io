15133187592f96e802786c14012c9bcde322fb25
usdaud <38377904+usdaud@users.noreply.github.com>
2024-06-24 00:20:04 +0300

    index page

--- _plugins/letter_page_generator.rb
+++ _plugins/letter_page_generator.rb
@@ -8,10 +8,13 @@ module Jekyll
       languages.each do |lang|
         pedia_path = File.join(site.source, lang, 'pedia')
         if Dir.exist?(pedia_path)
+          letters = []
           Dir.entries(pedia_path).select { |entry| File.directory?(File.join(pedia_path, entry)) && entry != '.' && entry != '..' }.each do |letter_dir|
             letter = letter_dir.downcase
+            letters << letter
             site.pages << LetterPage.new(site, site.source, "#{lang}/pedia", letter, lang)
           end
+          site.pages << PediaIndexPage.new(site, site.source, "#{lang}/pedia", letters, lang)
         end
       end
     end
@@ -29,6 +32,26 @@ module Jekyll
       self.data['layout'] = 'letter_index'
       self.data['title'] = "Topics starting with #{letter.upcase}"
       self.data['letter'] = letter
+      self.data['lang'] = lang
+    end
+  end
+
+  class PediaIndexPage < Page
+    def initialize(site, base, dir, letters, lang)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = "index.md"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'pedia_index.html')
+      self.data['layout'] = 'pedia_index'
+      self.data['title'] = case lang
+                           when 'en' then "Encyclopedia Index"
+                           when 'ru' then "Индекс Энциклопедии"
+                           when 'zh' then "百科索引"
+                           end
+      self.data['letters'] = letters
       self.data['lang'] = lang
     end
   end
