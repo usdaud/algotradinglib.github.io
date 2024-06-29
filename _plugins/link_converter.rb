24a1a441dcaa2b69187202ffd2e04d045e8d7447
usdaud <38377904+usdaud@users.noreply.github.com>
2024-06-29 23:28:11 +0300

    fixes

--- _plugins/link_converter.rb
+++ _plugins/link_converter.rb
@@ -6,7 +6,11 @@
     def generate(site)
       site.pages.each do |page|
         if page.output_ext == ".html"
-          page.output.gsub!(/(<a href=".*?)\.md(["'])/, '\1.html\2')
+          if page.output
+            page.output.gsub!(/(<a href=".*?)\.md(["'])/, '\1.html\2')
+          else
+            Jekyll.logger.warn "LinkConverter:", "No output for page: #{page.path}"
+          end
         end
       end
     end
