cec1358f5b1e05ca26f0eada792205002290dbc1
junsan1 <junsan1>
2024-06-29 16:56:48 +0300

    link_converter.rb

---
+++ _plugins/link_converter.rb
@@ -0,0 +1,14 @@
+﻿module Jekyll
+  class LinkConverter < Jekyll::Generator
+    safe true
+    priority :lowest
+
+    def generate(site)
+      site.pages.each do |page|
+        if page.output_ext == ".html"
+          page.output.gsub!(/(<a href=".*?)\.md(["'])/, '\1.html\2')
+        end
+      end
+    end
+  end
+end
