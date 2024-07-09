e413eb198814760e1f508a180f437281fcc43e0f
junsan1 <junsan1>
2024-07-09 10:40:39 +0300

    external links as blank

--- _plugins/link_converter.rb
+++ _plugins/link_converter.rb
@@ -12,11 +12,26 @@
     end
 
     def convert_links(page)
-      page.content.gsub!(/(\[.*?\])\(((?!http|www\.|\/|mailto:).*?)\.md\)/) do |match|
+      page.content.gsub!(/(\[.*?\])\((.*?)\)/) do |match|
         link_text = $1
-        link_path = $2
-        "#{link_text}(#{link_path}.html)"
+        link_url = $2
+
+        if internal_link?(link_url)
+          if link_url.end_with?('.md')
+            "#{link_text}(#{link_url.sub('.md', '.html')})"
+          else
+            "#{link_text}(#{link_url})"
+          end
+        else
+          "#{link_text}(#{link_url}){:target=\"_blank\" rel=\"noopener noreferrer\"}"
+        end
       end
+    end
+
+    private
+
+    def internal_link?(link_url)
+      !link_url.start_with?('http', 'https', 'www.', '/', 'mailto:')
     end
   end
 end
